dd70c30c131aba7bf6036dbb56341c7f2535f2d0
e1mo <git@e1mo.de>
2020-07-11 01:26:47 +0200

    Initial commit

---
+++ get_docs.py
@@ -0,0 +1,149 @@
+from bs4 import BeautifulSoup
+from pathlib import Path,PurePath
+import json
+import csv
+import requests
+
+def getDoctors(params = {}):
+    pages = -1
+    page = 0
+    doctors = []
+
+    while page < pages or pages == -1:
+        page += 1
+        print('Querying page {}/{}... '.format(page,pages), end='')
+        html = callSearchService({**{'page': page},**params}) 
+        soup = BeautifulSoup(html, features='html.parser')
+        if pages < 0:
+            pages = getPageCount(soup)
+
+        newdocs = extractDoctors(soup)
+        doctors.extend(newdocs)
+        print('{} doctors added. {} total'.format(len(newdocs), len(doctors)))
+
+    return doctors
+
+def getPageCount(soup: BeautifulSoup):
+    maxPage = 0
+    pages = soup.find(
+        'form',
+        class_="pagination"
+    )
+
+    if pages is None:
+        return 1
+
+    pages = pages.find_all(
+        'input',
+        class_="pagination-button",
+        attrs={'type':'submit'}
+    )
+
+    return len(pages)
+        
+def extractDoctors(soup: BeautifulSoup):
+    doctors = []
+
+    rawDoctors = soup.find(
+        'ul', class_='results'
+    ).find_all('li')
+
+    for doctor in rawDoctors:
+        doc = {}
+        a = doctor.find('a')
+        p = a.find('p')
+        p_strs = [pstr for pstr in p.stripped_strings]
+
+        doc['name'] = a.find('h3').string.strip()
+        doc['establishment'] = p_strs[0]
+        doc['field'] = p_strs[1]
+        doc['address'] = p_strs[2]
+        doc['url'] = 'https://www.kv-thueringen.de{}'.format(a.get('href'))
+
+        doc = extendDoctor(doc)
+        doctors.append(doc)
+
+    return doctors
+
+def extendDoctor(doc: dict):
+    listCounter = 0
+    headpos = 0
+
+    infoFieldMap = {
+        'Zusatzbezeichung:':    'additionalDesignation',
+        'Leistungsangebote:':    'serviceRange',
+        'Sonderverträge:':      'specialContracts'
+    }
+    headMap = {
+        'Telefon': 'telephone',
+        'Einrichtung': None,
+        'Weitere Ärzte': 'otherDocs',
+        'Wegbeschreibung': None
+    }
+
+    r = requests.get(doc['url'])
+    soup = BeautifulSoup(r.text, features='html.parser')
+    res = soup.find('div', class_='resultdetail')
+    lists = res.find_all('ul')
+    headings = res.find_all('h3')
+    infoFields = res.find_all(string=['Zusatzbezeichung:', 'Leistungsangebote:', 'Sonderverträge:'])
+
+    for field in infoFields:
+        if field not in infoFieldMap:
+            continue
+
+        key = infoFieldMap[field]
+        doc[key] = []
+        for li in lists[listCounter].find_all('li'):
+            doc[key].append(li.string)
+
+        listCounter += 1
+
+    ps = res.find_all('p')
+    del ps[0:1+len(infoFields)] # Strip the headings we've already dealt with
+    if "Sprechzeiten" in headings[0]:
+        del headings[:1]
+
+    for head in [head.string for head in headings]:
+        if head not in  headMap:
+            continue
+
+        key = headMap[head]
+
+        if key is None:
+            headpos += 1
+            continue
+
+        val = ps[headpos].text.strip()
+        doc[key] = val
+
+        headpos += 1
+
+    return doc
+
+    
+def callSearchService(params, url='https://www.kv-thueringen.de/arztsuche'):
+    data = {}
+    defaultParams = {
+        'search': '',
+        'place': '',
+        'radius': '',
+        'typeFachgebiet': '',
+        'typeSchwerpunkt': '',
+        'typeZusatzbezeichnung': '',
+        'typeGenehmigung': '',
+        'typeSelektivvertraege': '',
+        'filter': 1,
+        '__referrer][@action': 'list'
+    }
+    params = {**defaultParams, **params}
+    for key,val in params.items():
+        data['tx_t3kvclient_showclient[{}]'.format(key)] = val
+
+    return requests.post(url,data).text
+
+if __name__ == '__main__':
+    #doctors = getDoctors({'search': 'Tondt'})
+    doctors = getDoctors()
+    with open('out/doctors.json', 'w') as fp:
+        json.dump(doctors, fp)
---
+++ get_options.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup as Bs
+from pathlib import Path,PurePath
+import json
+import csv
+import requests
+
+def getOptionsFromSelect(id):
+    r = requests.get('https://www.kv-thueringen.de/arztsuche')
+    content = r.text
+    soup = Bs(content,features='html.parser')
+    rawAddDesignations = soup.find('select', id=id).find_all('option')
+    rawAddDesignations = (x for x in rawAddDesignations if x.get('value') != '')
+
+    addDesignations = {}
+    for option in rawAddDesignations:
+        # With some entries (detected at selFachgebiet > FA Radiologie)
+        # there are two numbers within the value field
+        # They are sperated by a ',', this looks like this: option value="44,86"
+        # We split the value and itterate over it to remove that annoying ,
+        for value in option.get('value').split(','):
+            addDesignations[int(value)] = option.contents
+
+    return dict(sorted(addDesignations.items(), key = lambda kv:kv[0]))
+   
+def getAdditionalDesignations():
+    return getOptionsFromSelect('selzusatzbezeichnung')
+
+def getServiceOffers():
+    return getOptionsFromSelect('selGenehmigung')
+
+def getSpecialContracts():
+    return getOptionsFromSelect('selSelektivvertraege')
+
+def writeToCSV(path,data,fieldnames=['num', 'title']):
+    Path(PurePath(path).parent).mkdir(parents=True,exist_ok=True)
+
+    with open (path, 'w', newline='') as f:
+        fw = csv.writer(f)
+
+        fw.writerow(['num', 'name'])
+        for key,des in data.items():
+            fw.writerow([key,des[0]])
+       
+
+if __name__ == '__main__':
+    ids = ['selFachgebiet', 'selSchwerpunkt', 'selzusatzbezeichnung', 'selGenehmigung', 'selSelektivvertraege']
+
+    for id in ids:
+        print(id)
+        writeToCSV('out/{}.csv'.format(id),getOptionsFromSelect(id))
